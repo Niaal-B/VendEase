5d14c43e0744a4eedd8c525d2f3f6002b202e9d5
Niaal-B <nihnihal504@gmail.com>
2025-12-17 00:07:12 +0530

    fix(backend): allowed any in login view

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,16 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+
+class PublicTokenObtainPairView(TokenObtainPairView):
+    permission_classes = [AllowAny]
+
+class PublicTokenRefreshView(TokenRefreshView):
+    permission_classes = [AllowAny]
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("vendors.urls")),
     path("api/", include("purchase_orders.urls")),
 
-    path("api/auth/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/auth/token/", PublicTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/auth/token/refresh/", PublicTokenRefreshView.as_view(), name="token_refresh"),
 ]
