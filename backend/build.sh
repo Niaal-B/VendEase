471a96d0a997249f8a648054b2c2f4e6b68ff7a2
Niaal-B <nihnihal504@gmail.com>
2025-12-16 23:55:20 +0530

    added file to create admin

--- backend/build.sh
+++ backend/build.sh
@@ -5,3 +5,8 @@ pip install -r requirements.txt
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
+# Create superuser if environment variables are set
+if [ -n "$DJANGO_SUPERUSER_PASSWORD" ]; then
+    python manage.py create_admin || true
+fi
+
